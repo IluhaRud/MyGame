db7413986acfd0e96854afc0871e6db3c9c96bf0
IluhaRud <redl2001@outlook.com>
2020-11-05 19:03:01 +0300

    First commit
    Commit my project

---
+++ Assets/Scripts/Diamondo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamondo : MonoBehaviour
+{
+    public Vector3 up, down;
+    public float speed = 0.5f;
+    public float speedRotation;
+    public Transform diamond;
+    public bool check;
+    // Start is called before the first frame update
+    void Start()
+    {
+        up = diamond.position;
+        down = new Vector3(diamond.position.x, diamond.position.y - 0.5f, diamond.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (check)
+        {
+            case true:
+                MoveDown();
+                if (diamond.position.y <= down.y)
+                    check = false;
+                break;
+
+            case false:
+                MoveUp();
+                if (diamond.position.y >= up.y)
+                    check = true;
+                break;
+        }
+
+        diamond.localEulerAngles += new Vector3(0, speedRotation * Time.deltaTime, 0);
+    }
+
+    void MoveUp()
+    {
+        gameObject.transform.position = new Vector3(diamond.position.x, diamond.position.y + speed * Time.deltaTime, diamond.position.z);
+    }
+
+    void MoveDown()
+    {
+        gameObject.transform.position = new Vector3(diamond.position.x, diamond.position.y - speed * Time.deltaTime, diamond.position.z);
+    }
+}
---
+++ Assets/Scripts/Reel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reel : MonoBehaviour
+{
+    public Transform reel;
+    public float speed = 1;
+    public float speedRotation = 1;
+    public bool check;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (check)
+        {
+            case true:
+                MoveRight();
+                if (reel.localPosition.y >= 0.9f)
+                    check = false;
+                break;
+
+            case false:
+                MoveLeft();
+                if (reel.localPosition.y <= -0.9f)
+                    check = true;
+                break;
+        }
+
+        reel.localEulerAngles += new Vector3(0, 0, -speedRotation * Time.deltaTime);
+    }
+
+    void MoveRight()
+    {
+        reel.transform.localPosition += new Vector3(0, speed * Time.deltaTime, 0);
+    }
+
+    void MoveLeft()
+    {
+        reel.transform.localPosition += new Vector3(0, -speed * Time.deltaTime, 0);
+    }
+}
