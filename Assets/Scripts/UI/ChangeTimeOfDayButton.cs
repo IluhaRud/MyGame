cb25ecd943d06e35e1cb129715398f81f9b93840
IluhaRud <redl2001@outlook.com>
2020-11-06 23:29:35 +0300

    UI
    Create simple UI

---
+++ Assets/Scripts/UI/ChangeTimeOfDayButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTimeOfDayButton : MonoBehaviour
+{
+    public Text textOfButton;
+    public GameObject _light;
+    public List<GameObject> streetLights = new List<GameObject>();
+    public bool IsEvening;
+
+    private void Start()
+    {
+        IsEvening = false;
+        textOfButton.text = "Afternoon";
+        streetLights.AddRange(GameObject.FindGameObjectsWithTag("StreetLight"));
+    }
+    public void ChangeTimeOfDay()
+    {
+        if (IsEvening == false)
+        {
+            textOfButton.text = "Evening";
+            _light.gameObject.SetActive(true);
+            for (int i = 0; i < streetLights.Count; i++)
+                streetLights[i].transform.GetChild(1).gameObject.SetActive(false);
+            IsEvening = true;
+            return;
+        }
+
+        if (IsEvening == true)
+        {
+            textOfButton.text = "Afternoon";
+            _light.gameObject.SetActive(false);
+            for (int i = 0; i < streetLights.Count; i++)
+                streetLights[i].transform.GetChild(1).gameObject.SetActive(true);
+            IsEvening = false;
+            return;
+        }
+    }
+
+}  
+
+
---
+++ Assets/Scripts/UI/InformationPanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationPanel : MonoBehaviour
+{
+    public Text textFPS;
+    public Text textDate;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textFPS.text = $"FPS: {(int)(1 / Time.deltaTime)}";
+        textDate.text = $"DATE: {DateTime.Now}";
+    }
+}
